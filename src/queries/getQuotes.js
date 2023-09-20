2b63185e9da73216346b75fd308e726a2f84d71f
awahab1116 <abdul.wahab@codistan.org>
2023-09-20 11:59:47 +0500

    update: vehicle filter and payment intent added

--- src/queries/getQuotes.js
+++ src/queries/getQuotes.js
@@ -1,6 +1,16 @@
 import checkMinMaxExists from "../utils/defaultMinMaxRange.js";
 import vehicleFilterQuery from "../utils/quoteVehicleFilters.js";
 
+function checkfilterObject(myObject) {
+  if (Object.keys(myObject).length > 0) {
+    console.log("object has keys ", myObject);
+    return true;
+  } else {
+    console.log("myObject does not have any keys");
+    return false;
+  }
+}
+
 export default async function getQuotes(
   context,
   {
@@ -20,37 +30,77 @@ export default async function getQuotes(
   console.log("in context queries boolean filter ", quoteBooleanFilters);
   console.log("in context queries string filter", quoteSimpleFilters);
 
-  const query = {
-    // "product.isDeleted": { $ne: true },
-    ...quoteBooleanFilters,
-    ...quoteSimpleFilters,
-  };
+  let query = [];
 
-  const selector = {};
+  if (checkfilterObject(quoteBooleanFilters)) {
+    query.push(quoteBooleanFilters);
+  }
+  if (checkfilterObject(quoteSimpleFilters)) {
+    query.push(quoteSimpleFilters);
+  }
+  // query.push(quoteSimpleFilters);
+
+  // const query = [
+  //   // "product.isDeleted": { $ne: true },
+  //   ...quoteBooleanFilters,
+  //   ...quoteSimpleFilters,
+  // ];
 
   if (searchQuery) {
-    query.$or = [
-      {
-        "quoteTo.city": {
-          $regex: new RegExp(searchQuery, "i"),
+    const vehicleIds = await collections.Vehicles.distinct("_id", {
+      $or: [
+        {
+          vehicleMake: {
+            $regex: new RegExp(searchQuery, "i"),
+          },
         },
-      },
-      {
-        "quoteTo.state": {
-          $regex: new RegExp(searchQuery, "i"),
+        {
+          vehicleModel: {
+            $regex: new RegExp(searchQuery, "i"),
+          },
         },
-      },
-      {
-        "quoteFrom.city": {
-          $regex: new RegExp(searchQuery, "i"),
+        {
+          vehicleYear: {
+            $regex: new RegExp(searchQuery, "i"),
+          },
         },
-      },
-      {
-        "quoteFrom.state": {
-          $regex: new RegExp(searchQuery, "i"),
+        {
+          type: {
+            $regex: new RegExp(searchQuery, "i"),
+          },
         },
-      },
-    ];
+      ],
+    });
+
+    query.push({
+      $or: [
+        {
+          vehicleId: {
+            $in: vehicleIds,
+          },
+        },
+        {
+          "quoteTo.city": {
+            $regex: new RegExp(searchQuery, "i"),
+          },
+        },
+        {
+          "quoteTo.state": {
+            $regex: new RegExp(searchQuery, "i"),
+          },
+        },
+        {
+          "quoteFrom.city": {
+            $regex: new RegExp(searchQuery, "i"),
+          },
+        },
+        {
+          "quoteFrom.state": {
+            $regex: new RegExp(searchQuery, "i"),
+          },
+        },
+      ],
+    });
   }
 
   console.log("1293810923 query is ", query);
@@ -60,12 +110,13 @@ export default async function getQuotes(
     const minPrice = floatRange.find((item) => item.name === "minPrice").value;
     const maxPrice = floatRange.find((item) => item.name === "maxPrice").value;
 
-    query[`price`] = {
-      $gte: parseFloat(minPrice),
-      $lte: parseFloat(maxPrice),
-    };
+    query.push({
+      price: {
+        $gte: parseFloat(minPrice),
+        $lte: parseFloat(maxPrice),
+      },
+    });
   }
-
   if (checkMinMaxExists(floatRange, "minDistance", "maxDistance")) {
     console.log("Both min and max distance exist");
 
@@ -76,25 +127,25 @@ export default async function getQuotes(
       (item) => item.name === "maxDistance"
     ).value;
 
-    query[`distance`] = {
-      $gte: parseFloat(minDistance),
-      $lte: parseFloat(maxDistance),
-    };
+    query.push({
+      distance: {
+        $gte: parseFloat(minDistance),
+        $lte: parseFloat(maxDistance),
+      },
+    });
   }
-
-  console.log("vehicle filter input is ", vehicleFilter);
 
   console.log("Query is ", query);
-  let filters = vehicleFilterQuery(vehicleFilter);
-  console.log("Vehicle Filters are ", filters);
+  // let filters = vehicleFilterQuery(vehicleFilter);
+  // console.log("Vehicle Filters are ", filters);
 
-  // Construct the final query object based on the provided filters
+  // // Construct the final query object based on the provided filters
 
-  if (filters) {
-    query[`vehicleId`] = {
-      $in: await Vehicles.distinct("_id", filters),
-    };
-  }
+  // if (filters) {
+  //   query[`vehicleId`] = {
+  //     $in: await Vehicles.distinct("_id", filters),
+  //   };
+  // }
 
   console.log("Query is ", query);
 
@@ -109,15 +160,21 @@ export default async function getQuotes(
     console.log("email is ", context.account.emails[0].address);
     console.log("This is simple user");
 
-    query["quotePersonEmail"] = context.account.emails[0].address;
+    query.push({
+      quotePersonEmail: context.account.emails[0].address,
+    });
   } else if (context?.account?.userRole == "admin") {
     console.log("in admin check quotes");
-    query["assignedTo.email"] = context.account.emails[0].address;
+    query.push({
+      "assignedTo.email": context.account.emails[0].address,
+    });
   } else {
     console.log("This is admin ", context?.account?.adminUIShopIds);
   }
 
   console.log("new query is ", query);
 
-  return Quotes.find(query);
+  return Quotes.find({
+    $and: query,
+  });
 }
